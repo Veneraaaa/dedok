c13fd6ec820f49205939ff25fd792df4c94257d0
Venera <kusmanova_venera01@mail.ru>
2023-03-14 14:01:01 +0500

    doen before replace

--- abc-js/practice/utils/string-utils/replace.js
+++ abc-js/practice/utils/string-utils/replace.js
@@ -1,13 +1,43 @@
+function isString(text, parName) {
+    if(typeof text !== 'string') throw Error (`argument${parName ?? ''} must be type of string`);
+  }
 /** Возвращает строку text, где первое вхождение search поменяно на target.
  * text: строка, копию которой нужно получить.
  * search: строка которое нужно поменять.
  * target: строка, на которую нужно поменять. */
 export function replace(text, search, target) {
-}
+    isString(text, ' text');
+    isString(search, ' search');
+    isString(target, ' target');
+
+    let  len = search.length;
+    if (!text.includes(search)) return text;
+    for (let i = 0; i < len; i++) {
+      i = text.indexOf(search, i);
+      let firstText = text.substring(0, i);
+      let secondText = text.substring(i + len);
+      if (i !== -1) {
+        text = firstText + target + secondText;
+        i += len;
+      } 
+    }
+    return text;
+  }
 
 /** Возвращает строку text, где все вхождения search поменяно на target.
  * text: строка, копию которой нужно получить.
  * search: строка которое нужно поменять.
  * target: строка, на которую нужно поменять. */
 export function replaceAll(text, search, target) {
+    isString(text, ' text');
+    isString(search, ' search');
+    isString(target, ' target');
+    if (!text.includes(search)) return text;
+    let  len = search.length;
+    let index = text.indexOf(search);
+    while (index !== -1) {
+        text = text.substr(0, index) + target + text.substr(index + len);
+        index = text.indexOf(search, index + target.length);
+    }
+    return text;
 }
--- abc-js/practice/utils/string-utils/trim.js
+++ abc-js/practice/utils/string-utils/trim.js
@@ -1,24 +1,36 @@
+import { reverse } from "./reverse.js";
+
 const TRIM_SYMBOLS = ' \n\t\v'; 
+function isString(text, parName) {
+  if(typeof text !== 'string') throw Error (`argument${parName ?? ''} must be type of string`);
+}
 /** Возвращает копию строки с удаленными символами со строки trimSymbols в начале строки.
  * По умолчанию удаляются символы пробела, табуляции и перевода строки.*/
 export function trimStart(text, trimSymbols=TRIM_SYMBOLS) {
-    let start = 0;
+    isString(text, ' text');
+    isString(trimSymbols, ' trimSymbols');
     for (let i = 0; i < text.length; i++) {
-    if (text[i] !== trimSymbols[i]) {
-      start = i;
-      break;
+    if (!trimSymbols.includes(text[i])) {
+      return text.substring(i);
     }
   } 
-  return text.substring(start);
+  return '';
 }
 
 /** Возвращает копию строки с удаленными символами со строки trimSymbols в конце строки.
  * По умолчанию удаляются символы пробела, табуляции и перевода строки.*/
 export function trimEnd(text, trimSymbols=TRIM_SYMBOLS) {
+  if(typeof trimSymbols !== 'string') throw Error ('argument trimSymbols must be type of string');
+  if (typeof text !== 'string') throw Error ('argument text must be type of string');
+  return reverse(trimStart(reverse(text), trimSymbols));
 }
 
 /** Возвращает копию строки с удаленными символами
  * со строки trimSymbols в начале и конце строки.
  * По умолчанию удаляются символы пробела, табуляции и перевода строки.*/
 export function trim(text, trimSymbols=TRIM_SYMBOLS) {
+  if(typeof trimSymbols !== 'string') throw Error ('argument trimSymbols must be type of string');
+  if (typeof text !== 'string') throw Error ('argument text must be type of string');
+  return trimStart(trimEnd(text, trimSymbols), trimSymbols);
+
 }
